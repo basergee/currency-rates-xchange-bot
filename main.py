6054b018380edf4a4d77ffbfe93da1811ff64056
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-03-27 20:36:38 +0500

    Добавил объект бота и реакцию на команды start и help

--- main.py
+++ main.py
@@ -3,3 +3,23 @@
 
 Здесь расположены работы с Telegram, функции-обработчики сообщений бота
 """
+
+import telebot
+
+import config
+
+
+def main():
+    bot.polling()
+
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет")
+
+
+if __name__ == "__main__":
+    main()
