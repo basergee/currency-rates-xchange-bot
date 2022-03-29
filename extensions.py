9173c5213b12dea108e0dd25e84810fa984e9cea
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-03-29 10:45:16 +0500

    Обновил запрос к API

--- extensions.py
+++ extensions.py
@@ -9,13 +9,14 @@ import json
 class Convertor:
     @staticmethod
     def get_price(base, quote, amount):
-        api_request = f'https://api.exchangerate.host/convert?from={base}&to={quote}'
+        api_request = f'https://api.exchangerate.host/convert?' \
+                      f'from={base}&to={quote}&amount={amount}'
         response = requests.get(api_request)
 
         # Можно вызвать response.json(), но в задании указано, что надо
         # использовать библиотеку JSON, поэтому делаем так
         data = json.loads(response.text)
-        return amount * data['info']['rate']
+        return data['result']
 
 
 # Проверим работу функции отдельно от остальной программы
