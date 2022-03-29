ad5c7147f9b8eb876e495c9477ce03bd8e8bd994
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-03-29 10:12:54 +0500

    Сделал запрос к API

--- extensions.py
+++ extensions.py
@@ -2,8 +2,19 @@
 Здесь расположены классы работы с API сервера, классы исключений и т.п.
 """
 
+import requests
+
 
 class Convertor:
     @staticmethod
     def get_price(base, quote, amount):
-        pass
+        api_request = f'https://api.exchangerate.host/convert?from={base}&to={quote}'
+        response = requests.get(api_request)
+
+        data = response.json()
+        return amount * data['info']['rate']
+
+
+# Проверим работу функции отдельно от остальной программы
+if __name__ == "__main__":
+    print(Convertor.get_price("USD", "RUB", 45))
