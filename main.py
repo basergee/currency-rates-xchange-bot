7e6b67b7609ccfcac96d6b6887ff5e4d5aa99235
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-03-31 12:10:40 +0500

    Изменил текст приветственного сообщения

--- main.py
+++ main.py
@@ -21,7 +21,17 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет")
+    msg = f"Здравствуйте, {message.from_user.first_name}\n\n" \
+          f"Здесь Вы можете узнать цену на некоторые валюты, отправив " \
+          f"сообщение с текстом:\n\n" \
+          f"<имя валюты, цену которой хотите узнать> <имя валюты, в " \
+          f"которой хотите узнать цену первой валюты> <количество первой " \
+          f"валюты>.\n\nНапример, сообщение \"доллар рубль 100\" выведет" \
+          f" цифру, которая показывает за сколько рублей можно купить 100 " \
+          f"долларов по текущему курсу.\n\n" \
+          f"Чтобы посмотреть список доступных валют введите команду /values"
+
+    bot.send_message(message.chat.id, msg)
 
 
 @bot.message_handler(commands=['values'])
